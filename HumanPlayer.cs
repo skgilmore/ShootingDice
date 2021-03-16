aaa8ca1e0dcef7699fe3b6263546d010c1362b24
Stephanie Gilmore <74026523+skgilmore@users.noreply.github.com>
2021-03-16 13:58:28 -0500

    Update HumanPlayer.cs

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,21 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
 
+        public override void Play(Player other)
+        {
+            Console.WriteLine("Pick a number any number : ");
+            Console.ReadKey();
+            string answer = Console.ReadLine();
+
+            base.Play(other);
+
+        }
     }
 }
