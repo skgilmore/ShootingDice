d7ae2109c6673ce81e6ebce315635e5699baeb88
Stephanie Gilmore <74026523+skgilmore@users.noreply.github.com>
2021-03-16 13:57:46 -0500

    Update CreativeSmackTalkingPlayer.cs

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
 
+
+
+
+
+        List<string> TauntItUp { get; set; } = new List<string>()
+        {
+            " Im about to taunt you",
+            "Dont test me!",
+            "Youre going down punk",
+            "Lady Luck is on my side",
+            "Finish him",
+            "AAnything you can do I can do better",
+            "Get some fool"
+        };
+
+        public override void Play(Player other)
+        {
+            base.Play(other);
+            int x = new Random().Next(6) + 1;
+            Console.WriteLine(TauntItUp[x]);
+
+
+
+        }
     }
+
 }
+
+
+
