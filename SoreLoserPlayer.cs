73c6437cb6a96be27a35af5a5560150c87ceafc4
Stephanie Gilmore <74026523+skgilmore@users.noreply.github.com>
2021-03-16 13:56:28 -0500

    Update SoreLoserPlayer.cs
    Update

--- SoreLoserPlayer.cs
+++ SoreLoserPlayer.cs
@@ -1,11 +1,37 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player that throws an exception when they lose to the other player
     // Where might you catch this exception????
-    public class SoreLoserPlayer
+    public class SoreLoserPlayer : Player
     {
-        
+
+
+
+        public override void Play(Player other)
+        {
+
+            int otherRoll = Roll();
+            int myRoll = other.Roll();
+
+
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else
+            {
+                throw new Exception("I never win!!! Whaaa!");
+            }
+
+        }
     }
 }
+
+
