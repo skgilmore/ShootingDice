2a0ae9a333eceb4819f05f73775cf0ff862f12ee
Stephanie Gilmore <74026523+skgilmore@users.noreply.github.com>
2021-03-16 14:00:44 -0500

    Update SmackTalkingPlayer.cs

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,22 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
         public string Taunt { get; }
+        public SmackTalkingPlayer(string taunt)
+        {
+            Taunt = taunt;
+        }
+        public override void Play(Player other)
+        {
+            base.Play(other);
+            Console.WriteLine("\"Boooooo\"");
+            // SmackTalkingPlayer.Taunt(taunt)
+        }
     }
 }
